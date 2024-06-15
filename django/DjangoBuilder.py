e18518bad394308cddd2ea789f79bfd047226724
Lucio Montero <lucioric2000@hotmail.com>
2024-06-15 15:18:55 -0600

    Added some extra code at the end of the urls.py file to match the code in the Nod-backend code

--- django/DjangoBuilder.py
+++ django/DjangoBuilder.py
@@ -14,12 +14,35 @@ class DjangoBuilder:
         self.imports = {"models": [],
                         "serializers": ["from rest_framework import serializers", "from . import models"],
                         "views": ["from rest_framework import viewsets, permissions, status, pagination", "from . import models, serializers", "from rest_framework.response import Response", "from django.utils.decorators import method_decorator", "from django.views.decorators.cache import cache_page", "from rest_framework.decorators import action"],
-                        "urls": ["from . import views", "from rest_framework.routers import DefaultRouter", "from django.urls import include, path"]}
+                        "urls": ["from django.conf import settings", "from . import views", "from rest_framework.routers import DefaultRouter", "from django.urls import include, path", "from django.contrib import admin", "from rest_framework.schemas import get_schema_view"]}
         self.functions_and_classes = {"models": [], "serializers": [], "views": ["""class CustomPagination(pagination.PageNumberPagination):
             pass"""], "urls": []}
-        self.code_at_end = {"models": [], "serializers": [], "views": [], "urls": ["""urlpatterns = [
-    path('', include(router.urls)),
-]"""]}
+        self.code_at_end = {"models": [], "serializers": [], "views": [], "urls": ["""
+
+\"\"\"
+path('schema/', get_schema_view(
+    title="Your Project",
+    description="API for all things …",
+    version="1.0.0"
+), name='openapi-schema')
+\"\"\"
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api', include(router.urls)),
+    #path('', include(router.urls)),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns.extend(router.urls)
+"""]}
         self.requirements = []
 
         # TODO: on CREATE / UPDATE, upsert any Foreign Key relationships when the full object is passed
--- examples/generated/urls.py
+++ examples/generated/urls.py
@@ -1,7 +1,11 @@
 
 ###OBJECT-ACTIONS-URLS-STARTS###
+from django.conf import settings
 from . import views
 from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+from django.contrib import admin
+from rest_framework.schemas import get_schema_view
 router = DefaultRouter()
 router.register(r'api/customer', views.CustomerViewSet, basename='customer')
 router.register(r'api/supplier', views.SupplierViewSet, basename='supplier')
@@ -10,5 +14,31 @@ router.register(r'api/meal', views.MealViewSet, basename='meal')
 router.register(r'api/plan', views.PlanViewSet, basename='plan')
 router.register(r'api/order_item', views.OrderItemViewSet, basename='order_item')
 router.register(r'api/order', views.OrderViewSet, basename='order')
+
+
+"""
+path('schema/', get_schema_view(
+    title="Your Project",
+    description="API for all things …",
+    version="1.0.0"
+), name='openapi-schema')
+"""
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api', include(router.urls)),
+    #path('', include(router.urls)),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns.extend(router.urls)
+
 
 ###OBJECT-ACTIONS-URLS-ENDS###
