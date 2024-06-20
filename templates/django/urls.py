14054592cd9483197ad1e9941de9f0357c11a994
Lucio Montero <lucioric2000@hotmail.com>
2024-06-19 22:43:33 -0600

    Changed the urls.py template to reflect the urls.py file of a Django app

--- templates/django/urls.py
+++ templates/django/urls.py
@@ -6,28 +6,7 @@ path('schema/', get_schema_view(
 ), name='openapi-schema')
 """
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('api', include(router.urls)),
-]
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-    # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns.extend(router.urls)
-
-
-
-
-
-
-
-
+urlpatterns = router.urls
 
 
 
