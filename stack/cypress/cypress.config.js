f8c3f41fb24698496cd98fa7041dd0d8d6fbe096
Eli Taylor <eli@taylormadetraffic.com>
2024-12-17 10:47:42 -0800

    video compression

--- stack/cypress/cypress.config.js
+++ stack/cypress/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   video: true,
   trashAssetsBeforeRuns:false,
+  videoCompression: true,
   baseUrl: process.env.REACT_APP_API_HOST,
   e2e: {
     excludeSpecPattern: 'cypress/e2e/examples',
