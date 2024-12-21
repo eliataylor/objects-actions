aedd9057e82376ddfd287b1532f30e4896cbc30a
Eli Taylor <eli@taylormadetraffic.com>
2024-12-20 18:34:44 -0800

    forgot to copy docker-compose.yml

--- clone.sh
+++ clone.sh
@@ -5,42 +5,68 @@ echo "Setting up $MACHINE_NAME at stackpath"
 
 # Directories to loop through
 STACK_DIRS=("cypress" "databuilder" "django" "k6" "reactjs")
+mkdir -p "$STACK_PATH/stack"
 
-# Copy directories and clean generated files
+# Copy directories into stack folder and clean generated files
 for dir in "${STACK_DIRS[@]}"; do
     if [ -d "$SCRIPT_DIR/stack/$dir" ]; then
-        cp -R "$SCRIPT_DIR/stack/$dir" "$STACK_PATH/$dir"
-        echo "Copied $dir to $STACK_PATH/$dir"
+
+        cp -R "$SCRIPT_DIR/stack/$dir" "$STACK_PATH/stack/$dir"
+        echo "Copied $dir to $STACK_PATH/stack/$dir"
 
         # Remove generated files
         case "$dir" in
             "cypress")
-                rm -rf "$STACK_PATH/cypress/node_modules" \
-                       "$STACK_PATH/cypress/cypress/fixtures/*" \
-                       "$STACK_PATH/cypress/cypress/downloads/*" \
-                       "$STACK_PATH/cypress/cypress/screenshots/*" \
-                       "$STACK_PATH/cypress/cypress/videos/*" \
-                       "$STACK_PATH/cypress/cypress/e2e/examples"
+                rm -rf "$STACK_PATH/stack/cypress/node_modules" \
+                       "$STACK_PATH/stack/cypress/cypress/fixtures/*" \
+                       "$STACK_PATH/stack/cypress/cypress/downloads/*" \
+                       "$STACK_PATH/stack/cypress/cypress/screenshots/*" \
+                       "$STACK_PATH/stack/cypress/cypress/videos/*" \
+                       "$STACK_PATH/stack/cypress/cypress/e2e/examples"
                 ;;
             "databuilder")
-                rm -rf "$STACK_PATH/databuilder/node_modules"
+                rm -rf "$STACK_PATH/stack/databuilder/node_modules"
                 ;;
             "django")
-                rm -rf "$STACK_PATH/django/.venv" \
-                       "$STACK_PATH/django/media/uploads" \
-                       "$STACK_PATH/django/newproject_app/migrations/*"
+                rm -rf "$STACK_PATH/stack/django/.venv" \
+                       "$STACK_PATH/stack/django/media/uploads" \
+                       "$STACK_PATH/stack/django/newproject_app/migrations/*"
                 ;;
             "k6")
-                rm -rf "$STACK_PATH/k6/results/*"
+                rm -rf "$STACK_PATH/stack/k6/results/*"
                 ;;
             "reactjs")
-                rm -rf "$STACK_PATH/reactjs/node_modules"
+                rm -rf "$STACK_PATH/stack/reactjs/node_modules"
                 ;;
         esac
     else
         echo "Warning: Directory $SCRIPT_DIR/stack/$dir does not exist. Skipping."
     fi
 done
+
+# Rename directories in stack/django/ if they include 'oaexample'
+for dir in "$STACK_PATH/stack/django"/*; do
+    if [[ "$dir" == *"oaexample"* ]]; then
+        new_dir=$(echo "$dir" | sed "s/oaexample/$PROJECT_NAME/g")
+        mv "$dir" "$new_dir"
+        echo "Renamed $dir to $new_dir"
+    fi
+done
+
+# Copy docker-compose.yml to $STACK_PATH
+if [ -f "$SCRIPT_DIR/docker-compose.yml" ]; then
+    cp "$SCRIPT_DIR/docker-compose.yml" "$STACK_PATH/docker-compose.yml"
+    echo "Copied docker-compose.yml to $STACK_PATH"
+else
+    echo "Error: docker-compose.yml not found in $SCRIPT_DIR"
+fi
+
+if [ -f "$ENV_FILE" ]; then
+    cp "$ENV_FILE" "$STACK_PATH/.env"
+    echo "Copied $ENV_FILE to $STACK_PATH/.env"
+else
+    echo "Error: ENV $ENV_FILE not found"
+fi
 
 # String replacements
 REPLACEMENTS=(
@@ -50,15 +76,6 @@ REPLACEMENTS=(
   "APasswordYouShouldChange:$REACT_APP_LOGIN_PASS"
   "oaexample:$PROJECT_NAME"
 )
-
-# Rename directories in stack/django/ if they include 'oaexample'
-for dir in "$STACK_PATH/django"/*; do
-    if [[ "$dir" == *"oaexample"* ]]; then
-        new_dir=$(echo "$dir" | sed "s/oaexample/$PROJECT_NAME/g")
-        mv "$dir" "$new_dir"
-        echo "Renamed $dir to $new_dir"
-    fi
-done
 
 export LC_ALL=C # Avoid issues with non-UTF-8 characters
 
@@ -86,7 +103,7 @@ if [ ! -f "$ssl_cert_path" ]; then
 fi
 
 # Django dependencies
-cd "$STACK_PATH/django"
+cd "$STACK_PATH/stack/django"
 if [ ! -d .venv ]; then
     python -m venv .venv
     echo "Created new virtual environment."
